afbc7478f4061c04c761946600ad20deb7063fa0
Nicolas van Kempen <nvankemp@gmail.com>
2022-10-20 23:14:11 -0400

    Remove GraalVM directory since benchmarks are identical to Java

--- Java-GraalVM/install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update
-sudo apt-get install gcc zlib1g-dev build-essential unzip
-
-wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.0.0/graalvm-ce-java8-linux-amd64-20.0.0.tar.gz
-tar -xvzf graalvm-ce-java8-linux-amd64-20.0.0.tar.gz
-
-sudo mkdir /usr/lib/graal
-sudo mv graalvm-ce-java8-20.0.0/ /usr/lib/graal
