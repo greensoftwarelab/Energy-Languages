0fe6c986bb8b6329b5ac670e3b3c28ea2c31db0b
Nicolas van Kempen <nvankemp@gmail.com>
2023-04-10 16:32:13 -0400

    Fix output directory

--- scripts/measure.py
+++ scripts/measure.py
@@ -33,7 +33,7 @@ def main(args):
             task = progress.add_task(f"[{language}]", total=total)
 
             for benchmark in benchmarks:
-                path = os.path.join(ROOT, args.output, language)
+                path = os.path.join(args.output, language)
                 if not os.path.exists(path):
                     os.makedirs(path)
 
@@ -41,9 +41,7 @@ def main(args):
                 for benchmark in benchmarks:
                     directory = os.path.join(ROOT, language, benchmark)
 
-                    json = os.path.join(
-                        ROOT, args.output, language, f"{benchmark}.json"
-                    )
+                    json = os.path.join(args.output, language, f"{benchmark}.json")
                     run_status = subprocess.run(
                         ["make", "measure"],
                         cwd=directory,
