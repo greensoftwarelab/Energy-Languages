166c817ed8b47e565f9494c32906e5f448c60edc
Nicolas van Kempen <nvankemp@gmail.com>
2023-05-01 16:13:02 -0400

    Add mean total

--- scripts/table.py
+++ scripts/table.py
@@ -3,8 +3,6 @@ import os
 import json
 import statistics
 
-import numpy as np
-
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
@@ -31,6 +29,14 @@ if __name__ == "__main__":
 
     benchmarks = sorted(list({b for l in data.values() for b in l.keys()}))
 
+    means = {
+        language: {
+            benchmark: statistics.geometric_mean([e["runtime"] for e in subdata])
+            for benchmark, subdata in data[language].items()
+        }
+        for language in LANGUAGES
+    }
+
     table = Table(title="Runtimes")
     table.add_column("Benchmark")
     for language in LANGUAGES:
@@ -42,24 +48,33 @@ if __name__ == "__main__":
             missing_benchmarks.append(benchmark)
             continue
 
-        baseline = statistics.geometric_mean(
-            [e["runtime"] for e in data[BASELINE][benchmark]]
-        )
-
         entries = []
         for language in LANGUAGES:
             if benchmark not in data[language]:
                 entries.append("")
             else:
-                value = statistics.geometric_mean(
-                    [e["runtime"] for e in data[language][benchmark]]
-                )
-                entries.append(f"{value / baseline:.2f}")
+                normalized = means[language][benchmark] / means[BASELINE][benchmark]
+                entries.append(f"{normalized:.2f}")
         table.add_row(*(benchmark, *entries))
 
     for benchmark in missing_benchmarks:
         text = Text(benchmark)
         text.stylize("strike")
         table.add_row(text)
+
+    per_language_baseline = statistics.geometric_mean(
+        [m for m in means[BASELINE].values()]
+    )
+    per_language_normalized_means = {
+        language: statistics.geometric_mean([m for m in means[language].values()])
+        / per_language_baseline
+        for language in LANGUAGES
+    }
+
+    table.add_row()
+    table.add_row(
+        "Geometric Mean",
+        *[f"{per_language_normalized_means[l]:.2f}" for l in LANGUAGES],
+    )
 
     Console().print(table)
