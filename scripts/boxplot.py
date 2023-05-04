a02a32779b0a9bc496625d0cbe742da8cef86dc0
Nicolas van Kempen <nvankemp@gmail.com>
2023-05-04 14:22:50 -0400

    Boxplot update

--- scripts/boxplot.py
+++ scripts/boxplot.py
@@ -8,7 +8,8 @@ import numpy as np
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_ROOT = os.path.join(ROOT, "data", "obelix96")
-LANGUAGES = ["JavaScript", "TypeScript"]
+LANGUAGES = ["C", "C++", "Rust"]
+# LANGUAGES = ["JavaScript", "TypeScript"]
 FORMAT = "png"
 
 
@@ -34,46 +35,58 @@ if __name__ == "__main__":
         for b in benchmarks
     ]
 
-    subdata = [
-        [r["runtime"] / normalizations[i] for r in data[language][benchmark]]
-        for i, benchmark in enumerate(benchmarks)
+    data = {
+        language: {
+            benchmark: [
+                r["runtime"] / normalizations[i] for r in data[language][benchmark]
+            ]
+            for i, benchmark in enumerate(benchmarks)
+        }
         for language in LANGUAGES
-    ]
-
-    delta = 0.175
-    positions = [i // 2 + (2 * (i % 2) - 1) * delta for i in range(len(subdata))]
+    }
 
     with plt.style.context("bmh"):
         fig, ax = plt.subplots()
-        fig.set_size_inches(12, 8)
-        bp = ax.boxplot(
-            subdata,
-            positions=positions,
-            widths=(2 * delta - delta / 2),
-            showfliers=False,
-        )
+        fig.set_size_inches(12, 6)
 
-        for i in range(len(subdata)):
-            color = "blue" if i % 2 == 0 else "red"
-            plt.setp(bp["boxes"][i], color=color)
-            plt.setp(bp["medians"][i], color=color)
-            plt.setp(bp["caps"][2 * i], color=color)
-            plt.setp(bp["caps"][2 * i + 1], color=color)
-            plt.setp(bp["whiskers"][2 * i], color=color)
-            plt.setp(bp["whiskers"][2 * i + 1], color=color)
+        d = 2  # Horizontal distance between benchmarks
+        w = 1  # Width for a single benchmark.
+
+        colors = {
+            language: plt.get_cmap("hsv")(i / len(LANGUAGES))
+            for i, language in enumerate(LANGUAGES)
+        }
+
+        for i, benchmark in enumerate(benchmarks):
+            m = i * d
+            x = np.linspace(m - w / 2, m + w / 2, len(LANGUAGES))
+
+            for j, language in enumerate(LANGUAGES):
+                y = data[language][benchmark]
+                ax.scatter(
+                    np.repeat(x[j], len(y)), y, color=colors[language], s=5, alpha=0.3
+                )
 
         ax.set_xticks(
-            [i for i in range(len(benchmarks))], labels=benchmarks, rotation=90
+            [i * d for i in range(len(benchmarks))], labels=benchmarks, rotation=30
         )
+        ax.set_xticks(
+            [(2 * i - 1) * d / 2 for i in range(1, len(benchmarks))], minor=True
+        )
+        ax.grid(axis="y", visible=False)
+        ax.grid(axis="x", which="major", visible=False)
+        ax.grid(axis="x", which="minor", visible=True)
         # ax.set_ylim(0, ax.get_ylim()[1])
-        ax.set_ylabel("Time [ms]")
-        ax.set_title(f"TODO")
+        ax.set_ylim(0, 6)
+        ax.set_xlim(-d / 2, (len(benchmarks) - 1) * d + d / 2)
+        ax.set_ylabel(f"Time (Normalized to {LANGUAGES[0]})")
+        ax.set_title(
+            f"Comparing benchmark runtimes for {', '.join(LANGUAGES[:-1])} and {LANGUAGES[-1]}"
+        )
 
-        (blue,) = ax.plot([1, 1], "b-")
-        (red,) = ax.plot([1, 1], "r-")
-        ax.legend((blue, red), LANGUAGES)
-        blue.set_visible(False)
-        red.set_visible(False)
+        for language, color in colors.items():
+            ax.plot([], [], color=color, label=language)
+        ax.legend()
 
         fig.tight_layout()
         plt.savefig(f"boxplot.{FORMAT}", format=FORMAT)
