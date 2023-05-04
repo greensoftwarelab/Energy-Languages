e991be80b8ed05f0566092402a9515f3889aa28a
Nicolas van Kempen <nvankemp@gmail.com>
2023-05-04 19:05:37 -0400

    Format

--- scripts/time_v_energy.py
+++ scripts/time_v_energy.py
@@ -1,15 +1,24 @@
 import collections
-import os
 import json
+import os
 import sys
+
 import matplotlib.pyplot as plt
 
 
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_ROOT = os.path.join(ROOT, "data", "obelix96")
 LANGUAGES = ["C", "C++", "Rust", "Java", "Go", "JavaScript", "TypeScript", "Python"]
-STYLES = {"C": 'black', "C++": 'turquoise', "Rust": 'brown', "Java": 'red',
-          "Go": 'orange', "JavaScript": 'green', "TypeScript": 'blue', "Python": 'purple'}
+STYLES = {
+    "C": "black",
+    "C++": "turquoise",
+    "Rust": "brown",
+    "Java": "red",
+    "Go": "orange",
+    "JavaScript": "green",
+    "TypeScript": "blue",
+    "Python": "purple",
+}
 
 
 def get_avg(langbench_pair: list[tuple]) -> tuple:
@@ -23,7 +32,7 @@ def get_avg(langbench_pair: list[tuple]) -> tuple:
 
 if __name__ == "__main__":
     argc = len(sys.argv)
-    view = 'default' if argc < 2 else sys.argv[1]
+    view = "default" if argc < 2 else sys.argv[1]
 
     data = collections.defaultdict(lambda: collections.defaultdict(list))
     for language in LANGUAGES:
@@ -36,22 +45,25 @@ if __name__ == "__main__":
             with open(path, "r") as file:
                 for line in file:
                     line = json.loads(line)
-                    if line['energy']['pkg'] < 0:
+                    # TODO: This is safe to remove once measurements are fixed.
+                    if line["energy"]["pkg"] < 0:
                         continue
-                    data[language][benchmark].append((line['runtime'], line['energy']['pkg']))
+                    data[language][benchmark].append(
+                        (line["runtime"], line["energy"]["pkg"])
+                    )
 
     benchmarks = sorted(list({b for l in data.values() for b in l.keys()}))
     plt.figure(figsize=(10, 7))
-    if view == 'medium':
+    if view == "medium":
         plt.axis([0, 40000, 0, 2000])
-    elif view == 'large':
+    elif view == "large":
         plt.axis([0, 10000, 0, 750])
-    elif view == 'logscale':
-        plt.xscale('log')
-        plt.yscale('log')
-    plt.title(f'Time v. Energy: {view} view')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Energy (J)')
+    elif view == "logscale":
+        plt.xscale("log")
+        plt.yscale("log")
+    plt.title(f"Time v. Energy: {view} view")
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Energy (J)")
     for language in data:
         times, energies = [], []
         for benchmark in data[language]:
@@ -59,6 +71,6 @@ if __name__ == "__main__":
             times.append(time)
             energies.append(energy)
         plt.scatter(times, energies, color=STYLES[language], label=language)
-    plt.legend(loc='upper left')
-    plt.savefig(f'time_v_energy_{view}.jpg')
+    plt.legend(loc="upper left")
+    plt.savefig(f"time_v_energy_{view}.jpg")
     plt.show()
