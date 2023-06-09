c74bd979bba0a29cd48379f7c9b6bb03ca250988
Nicolas van Kempen <nicovank@users.noreply.github.com>
2023-06-09 13:33:23 -0700

    Use `python3` specifically in `gen-input.sh`

--- gen-input.sh
+++ gen-input.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Generating input for k-nucleotide benchmark"
-python Python/fasta/fasta.py 25000000 > knucleotide-input25000000.txt
+python3 Python/fasta/fasta.py 25000000 > knucleotide-input25000000.txt
 
 echo "Generating input for reverse-complement benchmark"
-python Python/fasta/fasta.py 25000000 > revcomp-input25000000.txt
+python3 Python/fasta/fasta.py 25000000 > revcomp-input25000000.txt
 
 echo "Generating input for regex-redux benchmark"
-python Python/fasta/fasta.py 5000000 > regexredux-input5000000.txt
+python3 Python/fasta/fasta.py 5000000 > regexredux-input5000000.txt
