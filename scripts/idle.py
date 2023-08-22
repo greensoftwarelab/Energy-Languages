78668f3adb7ebbebf9c1d3041d7a58ab021bfa5b
Nicolas van Kempen <nvankemp@gmail.com>
2023-08-22 15:13:30 -0400

    Minor idle script changes

--- scripts/idle.py
+++ scripts/idle.py
@@ -1,19 +1,16 @@
-import os
+import argparse
 
 import numpy as np
 import pandas
-
-ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CSV_PATH = os.path.join(ROOT, "data", "obelix96", "idle.csv")
 
 
 def is_sorted(l):
     return all(l[i] <= l[i + 1] for i in range(len(l) - 1))
 
 
-if __name__ == "__main__":
+def main(args):
     data = pandas.read_csv(
-        CSV_PATH,
+        args.path,
         delimiter=",",
         names=["timestamp", "energy"],
         dtype={"timestamp": np.int64, "energy": np.float64},
@@ -29,3 +26,9 @@ if __name__ == "__main__":
     data = energy[1:] / np.diff(timestamp)
     print(f"Geometric mean: {np.exp(np.log(data).mean()):.2f} J")
     print(f"Sigma: {np.std(data):.2f} J")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str, required=True)
+    main(parser.parse_args())
