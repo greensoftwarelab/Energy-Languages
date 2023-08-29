558eacd5e69b8672e148156d572eb34f0e3d9721
Nicolas van Kempen <nvankemp@gmail.com>
2023-08-29 11:20:58 -0400

    Update table script

--- scripts/table.py
+++ scripts/table.py
@@ -1,40 +1,37 @@
+import argparse
 import collections
-import os
 import json
+import os
 import statistics
 
 from rich.console import Console
 from rich.table import Table
 from rich.text import Text
 
-ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_ROOT = os.path.join(ROOT, "data", "obelix96")
-LANGUAGES = ["C", "C++", "Rust", "Java", "Go", "JavaScript", "TypeScript", "Python"]
-BASELINE = LANGUAGES[0]
 
-
-def table(title, data, means):
+def table(title, args, data, means):
     table = Table(title=title)
     table.add_column("Benchmark")
-    for language in LANGUAGES:
+    for language in args.languages:
         table.add_column(
-            f"{language} / {BASELINE}" if language != BASELINE else language
+            f"{language} / {args.baseline}" if language != args.baseline else language
         )
 
+    benchmarks = sorted(list({b for l in data.values() for b in l.keys()}))
     missing_benchmarks = []
     for benchmark in benchmarks:
-        if benchmark not in data[BASELINE]:
+        if benchmark not in data[args.baseline]:
             missing_benchmarks.append(benchmark)
             continue
 
         entries = []
-        for language in LANGUAGES:
+        for language in args.languages:
             if benchmark not in data[language]:
                 entries.append("")
-            elif language == BASELINE:
+            elif language == args.baseline:
                 entries.append(f"{means[language][benchmark]:.2f}")
             else:
-                normalized = means[language][benchmark] / means[BASELINE][benchmark]
+                normalized = means[language][benchmark] / means[args.baseline][benchmark]
                 entries.append(f"{normalized:.2f}")
         table.add_row(*(benchmark, *entries))
 
@@ -44,27 +41,27 @@ def table(title, data, means):
         table.add_row(text)
 
     per_language_baseline = statistics.geometric_mean(
-        [m for m in means[BASELINE].values()]
+        [m for m in means[args.baseline].values()]
     )
     per_language_normalized_means = {
         language: statistics.geometric_mean([m for m in means[language].values()])
         / per_language_baseline
-        for language in LANGUAGES
+        for language in args.languages
     }
 
     table.add_row()
     table.add_row(
         "Geometric Mean",
-        *[f"{per_language_normalized_means[l]:.2f}" for l in LANGUAGES],
+        *[f"{per_language_normalized_means[l]:.2f}" for l in args.languages],
     )
 
     return table
 
 
-if __name__ == "__main__":
+def main(args):
     data = collections.defaultdict(lambda: collections.defaultdict(list))
-    for language in LANGUAGES:
-        LANGUAGES_ROOT = os.path.join(DATA_ROOT, language)
+    for language in args.languages:
+        LANGUAGES_ROOT = os.path.join(args.data_root, language)
         assert os.path.isdir(LANGUAGES_ROOT)
         for benchmark in os.listdir(LANGUAGES_ROOT):
             path = os.path.join(LANGUAGES_ROOT, benchmark)
@@ -75,11 +72,12 @@ if __name__ == "__main__":
                     line = json.loads(line)
                     data[language][benchmark].append(line)
 
-    benchmarks = sorted(list({b for l in data.values() for b in l.keys()}))
+    
 
     Console().print(
         table(
             "Runtime",
+            args,
             data,
             {
                 language: {
@@ -88,7 +86,7 @@ if __name__ == "__main__":
                     )
                     for benchmark, subdata in data[language].items()
                 }
-                for language in LANGUAGES
+                for language in args.languages
             },
         )
     )
@@ -96,6 +94,7 @@ if __name__ == "__main__":
     Console().print(
         table(
             "Energy consumption",
+            args,
             data,
             {
                 language: {
@@ -106,21 +105,34 @@ if __name__ == "__main__":
                     )
                     for benchmark, subdata in data[language].items()
                 }
-                for language in LANGUAGES
+                for language in args.languages
             },
         )
     )
 
-    Console().print(
-        table(
-            "Cycles",
-            data,
-            {
-                language: {
-                    benchmark: statistics.geometric_mean([e["cycles"] for e in subdata])
-                    for benchmark, subdata in data[language].items()
-                }
-                for language in LANGUAGES
-            },
-        )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-root", type=str, required=True)
+    parser.add_argument(
+        "--languages",
+        type=str,
+        nargs="+",
+        default=[
+            "C",
+            "C++",
+            "Rust",
+            "Go",
+            "Java",
+            "C#",
+            "JavaScript",
+            "TypeScript",
+            "PHP",
+            "Python",
+        ],
     )
+    parser.add_argument(
+        "--baseline",
+        type=str,
+        default="C"
+    )
+    main(parser.parse_args())
